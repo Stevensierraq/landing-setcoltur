28acc3f1847afc04d2c15d2b7e38e9c226849d28
stevensierraq <stevensierra.dev@gmail.com>
2019-01-08 17:52:15 -0500

    create-landing

---
+++ js/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+$(window).on('load', function() {
+	/*------------------
+		Preloder
+	--------------------*/
+	$(".loader").fadeOut();
+	$("#preloder").delay(400).fadeOut("slow");
+
+});
+
+$(document).ready(function(){
+    var top = $('header').offset().top;
+
+    $(window).on('scroll', function(){
+        if($(window).scrollTop() > top){
+            $('header').addClass('header-fixed');
+            $('#logo').addClass('none');
+            $('#logo-text').removeClass('none');
+        }else{
+            $('header').removeClass('header-fixed');
+            $('#logo-text').addClass('none');
+            $('#logo').removeClass('none');
+        }
+    });
+});
+
+  /*---  Scroll Home --*/
+  $( "#home" ).click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop: 0}, 1000);
+  })
+
+  /*---  Scroll Up --*/
+  $( ".up" ).click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop: 0}, 1000);
+  })
+    
